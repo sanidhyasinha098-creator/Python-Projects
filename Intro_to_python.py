993368b019464a518738373724cf85bd2ff7cba7
sanidhyasinha098-creator <sanidhyasinha098@gmail.com>
2026-06-04 17:17:49 +0530

    Introduction

---
+++ Intro_to_python.py
@@ -0,0 +1,35 @@
+print("Welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+
+#End argument of Print statement
+print("Welcome to ", end="*")
+
+#Activity 2
+x=5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input 
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nWelcome to Codingal")
+
+#Activity 3
+
+import keyword
+# print all the Python keywords
+print("Python keywords are...\n")
+print(keyword.kwlist)
