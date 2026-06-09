2cd0d4ce3e346e6017870d1fdd8eab4a082a40b9
sanidhyasinha098-creator <sanidhyasinha098@gmail.com>
2026-06-09 17:21:14 +0530

    Data Types of Python

---
+++ data_types.py
@@ -0,0 +1,54 @@
+# Activity 1
+
+# A = 5
+# print("Type of A: ", type(A))
+
+# B = 2.5
+# print("Type of B: ", type(B))
+
+# C = "Coding"
+# print("Type of C: ", type(C))
+
+# D = True
+# print("Type of D: ", type(D))
+
+# Activity 2
+
+# Name = "Sanidhya Sinha"
+# Age = 9
+# Is_Student=True
+# Weight = 20.70
+
+# Printing Different Variables and their Data Type
+# print("Name :", Name)
+# print("Data Type of Name is", type(Name))
+
+# print("Age :", Age)
+# print("Data Type of this Age is", type(Age))
+
+# print("Is_Student :", Is_Student)
+# print("Data Type of this Is_Student is", type(Is_Student))
+
+# print("Weight :", Weight)
+# print("Data Type of this Weight is", type(Weight))
+
+# # Type casting to convert the datatype of variables
+# print("\n After Type Casting....")
+# age = str(Age)
+# print(age)
+# print("Data Type of age is", type(age))
+# weight = int(Weight)
+# print(weight)
+# print("Data Type of Weight is", type(weight))
+
+# Activity 3
+
+#Input a Word
+# text = str(input("Enter a string: "))
+
+# Reverse String
+#Using Step value as -1 to Iterate in Reverse
+# revText = text[::-1]
+# text = revText
+# print("Reverse of Given String is:")
+# print(text)
