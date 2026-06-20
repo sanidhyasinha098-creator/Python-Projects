6751e10b4f4baf5974b1e0adbc4fa42cd941b6b8
sanidhyasinha098-creator <sanidhyasinha098@gmail.com>
2026-06-20 17:06:59 +0530

    Operators II in Python

---
+++ Operators_II.py
@@ -0,0 +1,73 @@
+#Activity 1
+
+# A = 10
+# B = 12
+# C = 0
+
+# if A and B and C:
+#     print("All the numbers have boolean value as True")
+# else:
+#     print("At least one of the numbers has boolean value as False")
+
+
+
+# A = 10
+# B = -10
+# C = 0
+
+# if A > 0 or B > 0:
+#     print("Either 1 of the number is Greater than 0")
+# else:
+#     print("No numbers is Greater than 0")
+
+# if B > 0 or C > 0:
+#     print("Either 1 of the number is Greater than 0")
+# else:
+#     print("No numbers is Greater than 0")
+
+
+#Activity 2
+# A = 10
+# B = 12
+# C = 12
+
+# print(not (A == B))
+
+# A = "Python"
+# B = "Coding"
+
+# if not (A == B):
+#     print(A, "and", B, "are different")
+
+# A = 4
+# B = 5
+
+# if not ((A == 1) == (B == 5)):
+#     print("A is not 1 and B is 5")
+
+# A = int(input("Enter a Number: "))
+
+# if not (A % 2 == 0):
+#     print(A, "is an Odd Number")
+
+#Activity 3
+
+# Height = float(input("Enter Your Height in cm: "))
+# Wegiht = float(input("Enter Your Weight in kg: "))
+
+# BMI = Wegiht/(Height/100)**2
+
+# print("Your BMI is: ", BMI)
+
+# if BMI <= 18.4:
+#     print("You are Underweight")
+# elif BMI <= 24.9:
+#     print("You are Healthy")
+# elif BMI <= 29.9:
+#     print("You are Overweight")
+# elif BMI <= 34.9:
+#     print("You are Obese")
+# elif BMI <= 39.9:
+#     print("You are Severely Obese")
+# else:
+#     print("You are Severely Obese")
