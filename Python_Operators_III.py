1eb602a46e6b965b7d0452dbd7ccd0fb911f5af6
sanidhyasinha098-creator <sanidhyasinha098@gmail.com>
2026-06-25 16:58:47 +0530

    Python Operators III

---
+++ Python_Operators_III.py
@@ -0,0 +1,91 @@
+# #Activity 1
+
+# X = 5
+
+# if (type(X) is int):
+#     print("True")
+# else:
+#     print("False")
+
+
+# X = 5.5
+
+# if (type(X) is not float):
+#     print("True")
+# else:
+#     print("False")
+
+
+# X = 20
+# Y = 20
+
+# if (X is Y):
+#     print("X & Y are the Same Identity")
+
+
+
+# Y = 30
+
+# if (X is not Y):
+#     print("X & Y Have Different Identities")
+
+# #Activity 2
+
+# A = 10
+# B = -10
+
+# print("A >> 1", A >> 1)
+# print("B >> 1", B >> 1)
+
+
+
+# A = 5
+# B = -10
+
+# print("A << 1", A << 1)
+# print("B << 1", B << 1)
+
+#Activity 3
+
+# print("Enter Marks Optained in 5 Subjects: ")
+
+# Mark_1 = int(input())
+# Mark_2 = int(input())
+# Mark_3 = int(input())
+# Mark_4 = int(input())
+# Mark_5 = int(input())
+
+# Total_Marks = Mark_1 + Mark_2 + Mark_3 + Mark_4 + Mark_5
+# Avg = int(Total_Marks / 5)
+
+# Valid_Range = range(0, 101)
+
+# if Avg not in Valid_Range:
+#     print("Invalid Marks")
+
+# elif Avg in range(91, 101):
+#     print("Grade: A1")
+
+# elif Avg in range(81, 91):
+#     print("Grade: A2")
+
+# elif Avg in range(71, 81):
+#     print("Grade: B1")
+
+# elif Avg in range(61, 71):
+#     print("Grade: B2")
+
+# elif Avg in range(51, 61):
+#     print("Grade: C1")
+
+# elif Avg in range(41, 51):
+#     print("Grade: C2")
+
+# elif Avg in range(33, 41):
+#     print("Grade: D")
+
+# elif Avg in range(21, 33):
+#     print("Grade: E1")
+
+# elif Avg in range(0, 21):
+#     print("Grade: E2")
